48ece5d56726d5f2429976bcde54205920d1c930
Alfaza <alfazasatria8@gmail.com>
2019-09-02 15:18:16 +0700

    Add Method OnStart dll ()

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package id.ac.polinema.androidlifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +13,35 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 	}
 
-	// TODO: tambahkan callback onStart() di sini
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Toast.makeText(this, "ON APP START", Toast.LENGTH_SHORT).show();
+	}
 
+	@Override
+	protected void onStop () {
+		super.onStop();
+		Toast.makeText(this, "App On Stop", Toast.LENGTH_SHORT).show();
+	}
 
-	// TODO: tambahkan callback onStop() di sini
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Toast.makeText(this, "App On Pause", Toast.LENGTH_SHORT).show();
+	}
 
-	// TODO: lengkapi callback untuk onRestart() onResume() onPause() onDestroy()
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Toast.makeText(this, "App On Resume", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Toast.makeText(this, "App On Destroy", Toast.LENGTH_SHORT).show();
+	}
+
+	
 }
