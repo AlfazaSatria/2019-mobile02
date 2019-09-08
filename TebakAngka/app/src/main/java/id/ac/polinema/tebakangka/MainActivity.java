cde153ed64d1347c1d1e6456b10fd47295ed89f3
Alfaza <alfazasatria8@gmail.com>
2019-09-09 01:13:05 +0700

    Make Random number

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -1,36 +1,68 @@
 package id.ac.polinema.tebakangka;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
-	//  TODO: deklarasikan variabel di sini
+	Button btnTebak;
+	EditText etAngka;
+	int angkaRandom = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		// TODO: bind layout di sini
+
+		btnTebak = (Button) findViewById(R.id.btnTebak);
+		etAngka = (EditText) findViewById(R.id.etAngka);
+		angkaRandom = buatAngkaRandom(1, 3);
+		btnTebak.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				if (!etAngka.equals("")) {
+					tebakAngka();
+				}
+			}
+		});
 	}
 
-	// TODO: generate angka random di sini
-	private void initRandomNumber() {
-
+	private void tebakAngka(){
+		int angkaTebakan = Integer.parseInt(etAngka.getText().toString());
+		if (angkaTebakan == angkaRandom){
+			tampilkanPopup("tebakan anda benar");
+		} else {
+			tampilkanPopup("Tebakan anda salah");
+		}
+		angkaRandom = buatAngkaRandom(1, 3);
 	}
 
-	public void handleGuess(View view) {
-		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+	private int buatAngkaRandom(int angkaMin, int angkaMax){
+		Random rand = new Random();
+		int randomNum = rand.nextInt(angkaMax - angkaMin) + angkaMin;
+		return randomNum;
 	}
 
-	public void handleReset(View view) {
-		// TODO: Reset tampilan
+	private void tampilkanPopup(String pesan){
+		AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+		builder.setMessage(pesan+", jawaban : "+angkaRandom).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialogInterface, int i) {
+				etAngka.setText("");
+			}
+		});
+
+		// Create the AlertDialog
+		AlertDialog dialog = builder.create();
+		dialog.show();
 	}
+
 }
+
